23c76b823d1169123c0d41f37f2a75985b1fe9ef
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-22 16:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,19 +14,32 @@ function addToCart(item) {
  var itemPrice = Math.floor(Math.random()*100)
  + 1;
  cart.push({[item]: itemPrice});
- console.log([`${item} has been added to your cart.`]);
+ console.log(`${item} has been added to your cart.`);
  return cart;
 
 }
 
-function viewCart() {
-  // write your code here
-  for (var i = 0; i < cart.length; i++) {
-    cart[i];
+function viewCart(){
+   if (cart.length === 0) {
+      console.log("Your shopping cart is empty.");
+    } else {
 
-    console.log (`In your cart, you have bananas at ${Object.values(cart[i])},
-    pancake batter at ${Object.values(cart[i])}, and eggs at ${Object.values(cart[i])}.`);
+   if (cart.length === 1) {
+      console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])[0]}.`);
+
+    } else if  (cart.length === 2) {
+      console.log(cart);
+      console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`);
+    } else if (cart.length >= 3 ) {
+       for (var i=0; i < cart.length; i++) {
+    cart[i];
+      console.log(`In your cart, you have ${Object.keys(cart[i])} at $${Object.values(cart[i])[0]}, ${Object.keys(cart[i])} at $${Object.values(cart[i])[1]}, and ${Object.keys(cart[i])} at $${Object.values(cart[i])[2]}.`);
+    }
+
+     }
   }
+
+
 }
 
 function total() {
@@ -40,9 +53,9 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  if (cart.hasOwnProperty("koodi")) {
+  if (cart.hasOwnProperty(cart[item] !== item)) {
     console.log("That item is not in your cart.");
-  } else if (cart.hasOwnProperty(item)) {
+  } else if (cart.hasOwnProperty(cart[item])) {
     cart[item].pop();
 
   }
