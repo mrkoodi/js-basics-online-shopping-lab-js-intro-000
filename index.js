6d6d4ed18c87b4f41c0d64c153b5b84c12a44bad
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-23 01:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,13 +63,10 @@ function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
   if (cart[i].hasOwnProperty(item)) {
-    cart.splice(0, 1);
+    cart.splice(i, 1);
     return cart;
-  } else if (cart.hasOwnProperty(item)) {
-  
-    return cart;
-  }
-  
+  } 
+
   }
   console.log("That item is not in your cart.");
 }
